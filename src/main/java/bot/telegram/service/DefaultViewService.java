f5c7f2ffc309166feb92d1c77ae7f666a1b2864d
kottragu <alexklim0120@gmail.com>
2021-06-05 11:18:50 +0300

    feat

--- src/main/java/bot/telegram/service/DefaultViewService.java
+++ src/main/java/bot/telegram/service/DefaultViewService.java
@@ -4,6 +4,7 @@ import bot.telegram.entity.Event;
 import bot.telegram.service.interfaces.ViewService;
 import org.springframework.stereotype.Service;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -15,13 +16,26 @@ public class DefaultViewService implements ViewService {
         SimpleDateFormat dateFormat = new SimpleDateFormat("E, dd.MM.yyyy");
         StringBuilder result = new StringBuilder(dateFormat.format(new Date()));
         Collections.sort(events);
+
+        Calendar calendar = Calendar.getInstance();
+        int hours = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
         for (Event e: events) {
+            if (e.getTime().getHours() > hours){
+                add(e, result);
+            } else if (e.getTime().getHours() == hours && e.getTime().getMinutes() >= minute) {
+                add(e, result);
+            }
+        }
+        return result.toString();
+    }
+
+    private void add(Event e, StringBuilder result) {
             result.append("\n_");
             result.append(e.getTime()).append(" ").append(e.getTitle()).append(" ");
             if (e.getDescription() != null)
                 result.append(e.getDescription());
             result.append("_");
-        }
-        return result.toString();
     }
 }
